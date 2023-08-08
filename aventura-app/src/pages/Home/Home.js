a18d3d4d8e33589fa6fa95ec5597f8e7d9374c37
アミール　モハメド <mariemosalca@Amirs-MacBook.local>
2023-08-07 19:54:49 -0500

    added cards component.

--- aventura-app/src/pages/Home/Home.js
+++ aventura-app/src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Home.css'
 import Banner from '/Users/amir/Desktop/projects/aventura-app-react/aventura-app-react/aventura-app/src/components/Banner/Banner.js'
-import { Card } from '@mui/material'
+// import { Card } from '@mui/material'
+import Card from '/Users/amir/Desktop/projects/aventura-app-react/aventura-app-react/aventura-app/src/components/Card/Card.js'
 
 function Home() {
   return (
@@ -11,12 +12,12 @@ function Home() {
         <Card
           scr='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw8u-OiQI12PmWZHt1DFn3ASrWRNLZOV-ieg&usqp=CAU'
           title='Aventura Experiences'
-          description='Put these Aventura experiences on your bucket, the Aventura never stops.'
+          description='Put these Aventuras on your bucket, the Aventura never stops.'
         />
-          <Card
-            scr='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWEzbz6O5Ei1yR6dKuMEQ4ZxPUiAR8ME-oycQxlyW5__WcYCyTGxPzRavBb8zi2BhWkYA&usqp=CAU'
-            title='Unique stays'
-            description="Whether it's traditional or exotic, it will definitely be a stay to remember." />
+        <Card
+          scr='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWEzbz6O5Ei1yR6dKuMEQ4ZxPUiAR8ME-oycQxlyW5__WcYCyTGxPzRavBb8zi2BhWkYA&usqp=CAU'
+          title='Unique stays'
+          description="Whether it's traditional or exotic, it will definitely be a stay to remember." />
         <Card
           scr='https://i.etsystatic.com/42569828/r/il/4794e4/4806640032/il_1588xN.4806640032_kwl8.jpg'
           title='Private homes'
